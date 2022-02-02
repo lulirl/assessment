0f8204519e7f15172e95b090cec7ee4fb593e09a
lulirl <luciaruedalaje@192.168.1.7>
2022-02-01 23:23:20 -0300

    starting task 4

--- screens/QrList.jsx
+++ screens/QrList.jsx
@@ -4,10 +4,19 @@ import {StyleSheet, View, Text,FlatList, TextInput} from "react-native"
 
 function QrList( ) {
     var qrStrings = useSelector((state) => state.QrData);
-  
+    const [search, setSearch] = useState("")
     return ( 
        <View style={styles.body}>
-        <Text>Qr List</Text>
+        <TextInput style={styles.input}
+        placeholder="search..."
+        onChangeText={(value)=> setSearch(value)}/>
+        <Text>{search}</Text>
+        <FlatList
+        keyExtractor={(item, index) => index.toString()}
+        data={qrStrings}
+        renderItem={({item})=> (
+            <Text style={styles.item}>{item}</Text>
+        )}/>
        </View>
      );
 }
@@ -17,8 +26,20 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center"
     }, 
-  
-    
+    input:{
+        borderWidth: 1, 
+        borderColor: "black",
+        padding: 8, 
+        margin: 10,
+        width: 200 
+
+    }, 
+    item: {
+        marginTop: 24,
+        padding: 30,
+        backgroundColor: "blue",
+        fontSize: 24
+    }
 })
 
 export default QrList
